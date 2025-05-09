bb7c3227c4aef1293ef584660c5f2e912c806809
CharLamp10 <xaralabos10@hotmail.gr>
2025-05-09 16:31:34 +0400

    Update

--- result_scripts/prepare_for_BrainNetViewer.R
+++ result_scripts/prepare_for_BrainNetViewer.R
@@ -3,71 +3,9 @@ library(RcppCNPy)
 library(data.table)
 
 
-path_varconet_aicha_80 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_ICC_80_samples.csv"
-data_varconet_aicha_80 <- as.matrix(fread(path_varconet_aicha_80)[["ICC"]])
-path_varconet_aicha_200 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_AICHA_ICC_200_samples.csv"
-data_varconet_aicha_200 <- as.matrix(fread(path_varconet_aicha_200)[["ICC"]])
-path_varconet_aicha_320 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_AICHA_ICC_320_samples.csv"
-data_varconet_aicha_320 <- as.matrix(fread(path_varconet_aicha_320)[["ICC"]])
-path_varconet_aal_80 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_AAL_ICC_80_samples.csv"
-data_varconet_aal_80 <- as.matrix(fread(path_varconet_aal_80)[["ICC"]])
-path_varconet_aal_200 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_AAL_ICC_200_samples.csv"
-data_varconet_aal_200 <- as.matrix(fread(path_varconet_aal_200)[["ICC"]])
-path_varconet_aal_320 <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_AAL_ICC_320_samples.csv"
-data_varconet_aal_320 <- as.matrix(fread(path_varconet_aal_320)[["ICC"]])
-
-data_varconet_aicha = (data_varconet_aicha_80 + data_varconet_aicha_200 + data_varconet_aicha_320)/3
-data_varconet_aal = (data_varconet_aal_80 + data_varconet_aal_200 + data_varconet_aal_320)/3
-
-top_values <- sort(data_varconet_aicha, decreasing = TRUE)[1:50]
-data_varconet_aicha <- ifelse(data_varconet_aicha %in% top_values, data_varconet_aicha, 0)
-
-top_values <- sort(data_varconet_aal, decreasing = TRUE)[1:50]
-data_varconet_aal <- ifelse(data_varconet_aal %in% top_values, data_varconet_aal, 0)
-
-mat_varconet_aicha <- matrix(0, nrow = 384, ncol = 384)
-tri_idx <- which(upper.tri(mat_varconet_aicha))
-mat_varconet_aicha[tri_idx] <- data_varconet_aicha
-mat_varconet_aicha <- mat_varconet_aicha + t(mat_varconet_aicha)
-
-mat_varconet_aal <- matrix(0, nrow = 166, ncol = 166)
-tri_idx <- which(upper.tri(mat_varconet_aal))
-mat_varconet_aal[tri_idx] <- data_varconet_aal
-mat_varconet_aal <- mat_varconet_aal + t(mat_varconet_aal)
-
-aal_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\AAL3_coords.npy")
-aicha_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\AICHA_coords.npy")
-
-nonzero_rows <- apply(mat_varconet_aicha, 1, function(row) any(row != 0))
-row_indices <- which(nonzero_rows)
-aicha_coords <- aicha_coords[nonzero_rows, , drop = FALSE]
-mat_varconet_aicha <- mat_varconet_aicha[nonzero_rows,nonzero_rows]
-
-nonzero_rows <- apply(mat_varconet_aal, 1, function(row) any(row != 0))
-row_indices <- which(nonzero_rows)
-aal_coords <- aal_coords[nonzero_rows, , drop = FALSE]
-mat_varconet_aal <- mat_varconet_aal[nonzero_rows,nonzero_rows]
-
-write_brainNet(
-  aicha_coords,
-  node.size = rep(0, length(aicha_coords)),
-  mat_varconet_aicha,
-  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\',
-  name = 'brainNet_AICHA'
-)
-
-write_brainNet(
-  aal_coords,
-  node.size = rep(0, length(aal_coords)),
-  mat_varconet_aal,
-  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\',
-  name = 'brainNet_AAL'
-)
-
-
-path_varconet_aal_abide <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\feature_importance\\AAL_feature_importance.csv"
+path_varconet_aal_abide <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\feature_importance\\AAL_feature_importance.csv"
 data_varconet_aal_abide <- as.matrix(fread(path_varconet_aal_abide))
-path_varconet_aicha_abide <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\feature_importance\\AICHA_feature_importance.csv"
+path_varconet_aicha_abide <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\feature_importance\\AICHA_feature_importance.csv"
 data_varconet_aicha_abide <- as.matrix(fread(path_varconet_aicha_abide))
 
 top_values <- sort(data_varconet_aal_abide, decreasing = TRUE)[1:20]
@@ -86,8 +24,8 @@ tri_idx <- which(upper.tri(mat_varconet_aal))
 mat_varconet_aal[tri_idx] <- data_varconet_aal_abide
 mat_varconet_aal <- mat_varconet_aal + t(mat_varconet_aal)
 
-aal_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\AAL3_coords.npy")
-aicha_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\AICHA_coords.npy")
+aal_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\AAL3_coords.npy")
+aicha_coords <- npyLoad("C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\AICHA_coords.npy")
 
 nonzero_rows <- apply(mat_varconet_aicha, 1, function(row) any(row != 0))
 row_indices <- which(nonzero_rows)
@@ -103,7 +41,7 @@ write_brainNet(
   aicha_coords,
   node.size = rep(0, length(aicha_coords)),
   mat_varconet_aicha,
-  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNetV2\\feature_importance\\',
+  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\feature_importance\\',
   name = 'brainNet_AICHA_ASD'
 )
 
@@ -111,7 +49,7 @@ write_brainNet(
   aal_coords,
   node.size = rep(0, length(aal_coords)),
   mat_varconet_aal,
-  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNetV2\\feature_importance\\',
+  directory = 'C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_ABIDEI\\feature_importance\\',
   name = 'brainNet_AAL_ASD'
 )
 
--- result_scripts/rex_plots_2.R
+++ result_scripts/rex_plots_2.R
@@ -2,48 +2,42 @@ library(data.table)
 library(ReX)
 library(ggplot2)
 
-path_pcc <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\PCC_ICC_80_samples.csv"
+path_pcc <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_HCP\\ReX_files\\PCC_ICC_80_samples.csv"
 data_pcc <- fread(path_pcc)
-path_varconet <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarCoNetV2_ICC_80_samples.csv"
+path_varconet <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_HCP\\ReX_files\\VarCoNet_ICC_80_samples.csv"
 data_varconet <- fread(path_varconet)
-path_vae <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VAE_ICC_80_samples.csv"
+path_vae <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_HCP\\ReX_files\\VAE_ICC_80_samples.csv"
 data_vae <- fread(path_vae)
-path_ae <- "C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\AE_ICC_80_samples.csv"
+path_ae <- "C:\\Users\\100063082\\Desktop\\VarCoNet_results\\results_HCP\\ReX_files\\AE_ICC_80_samples.csv"
 data_ae <- fread(path_ae)
 
 data_pcc$group <- "PCC"
-data_varconet$group <- "VarCoNet-V2"
+data_varconet$group <- "VarCoNet"
 data_vae$group <- "(Lu et al., 2024)"
 data_ae$group <- "(Cai et al., 2021)"
 
 combined_data <- rbind(data_pcc, data_varconet)
 p_varconet_pcc_1 <- rex_plot.var.field.n(combined_data, size.point = 2, alpha.density = 0.3)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_PCC.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_PCC.png")
 df_VarPairedComp <- icc_gradient_flow(as.matrix(data_varconet[,"sigma2_w"]), as.matrix(data_varconet[,"sigma2_b"]), as.matrix(data_pcc[,"sigma2_w"]), as.matrix(data_pcc[,"sigma2_b"]))
 df_VarPairedComp$contrast <- "VarCoNet-V2 - PCC"
 p_varconet_pcc_2 <- rex_plot.icc.gradient.norm(df_VarPairedComp,show.contour = FALSE)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_PCC_grad_norm.png")
-p_varconet_pcc_3 <-rex_plot.icc.gradient.hist(df_VarPairedComp)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_PCC_grad_hist.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_PCC_grad_norm.png")
 
 combined_data <- rbind(data_vae, data_varconet)
 p_varconet_vae_1 <- rex_plot.var.field.n(combined_data, size.point = 2, alpha.density = 0.3)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_VAE.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_VAE.png")
 df_VarPairedComp <- icc_gradient_flow(as.matrix(data_varconet[,"sigma2_w"]), as.matrix(data_varconet[,"sigma2_b"]), as.matrix(data_vae[,"sigma2_w"]), as.matrix(data_vae[,"sigma2_b"]))
 df_VarPairedComp$contrast <- "VarCoNet-V2 - VAE"
 p_varconet_vae_2 <- rex_plot.icc.gradient.norm(df_VarPairedComp,show.contour = FALSE)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_VAE_grad_norm.png")
-p_varconet_vae_3 <-rex_plot.icc.gradient.hist(df_VarPairedComp)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_VAE_grad_hist.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_VAE_grad_norm.png")
 
 combined_data <- rbind(data_ae, data_varconet)
 p_varconet_ae_1 <- rex_plot.var.field.n(combined_data, size.point = 2, alpha.density = 0.3)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_AE.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_AE.png")
 df_VarPairedComp <- icc_gradient_flow(as.matrix(data_varconet[,"sigma2_w"]), as.matrix(data_varconet[,"sigma2_b"]), as.matrix(data_ae[,"sigma2_w"]), as.matrix(data_ae[,"sigma2_b"]))
 df_VarPairedComp$contrast <- "VarCoNet-V2 - AE"
 p_varconet_ae_2 <- rex_plot.icc.gradient.norm(df_VarPairedComp,show.contour = FALSE)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_AE_grad_norm.png")
-p_varconet_ae_3 <-rex_plot.icc.gradient.hist(df_VarPairedComp)
-ggsave("C:\\Users\\100063082\\Desktop\\VarCoNetV2\\rex_results\\VarVoNetV2_AE_grad_hist.png")
+ggsave("C:\\Users\\100063082\\Desktop\\VarCoNet-V2\\paper_plots\\VarCoNet_AE_grad_norm.png")
 
 
